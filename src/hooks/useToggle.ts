64b1a26b9170bd9ff29b8828e74432948205033c
Łukasz K <maczugapl@gmail.com>
2020-10-05 22:46:51 +0200

    Move hooks from utils to components.

--- src/hooks/useToggle.ts
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-export const useToggle = (initialState = false) => {
-  const [isOpen, setIsOpen] = React.useState<boolean>(initialState);
-  const toggle = () => setIsOpen(v => !v);
-
-  return {isOpen, toggle, setIsOpen};
-};
