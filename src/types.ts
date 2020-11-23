7681b63240bb04c28b5a3a42a30cccaa1674488f
Łukasz K <maczugapl@gmail.com>
2020-11-23 22:29:50 +0100

    Multi-value select initial works

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,6 @@
+
 export type Dictionary<T> = Record<string, T>;
 
 export type ArrayValue<T extends readonly unknown[]> = T extends ReadonlyArray<infer ElementType> ? ElementType : never
+
+export type USDefVal<T> = T | (() => T);
